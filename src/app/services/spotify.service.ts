3668f8c7d0110881a1f84375dc4cd63b35ed5609
Samuel Ruiz <samuel.ruiz@aktios.com>
2020-02-27 21:00:16 +0100

    añadimos funcionalidad obtener artista en el servicio en el componente y mostramos por html

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -19,4 +19,12 @@ export class SpotifyService {
     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers});
 
    }
+
+   getArtista(termino: string){
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQD6WG2twOz4i-05t9w8yw4_w-b5Y5QfqfLhFEFXirqBFYDFhtBcm35XTw7jHQ_kYfnM4CC76iLttuTFG6w'
+    })
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, {headers});
+   }
 }
